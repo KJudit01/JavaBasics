e68c2cfe8ce3abdb66e3eb2c0a010c0a1b66f654
Kiss Judit <kijudit77@gmail.com>
2023-06-13 21:08:48 +0200

    FizzBuzz

--- FizzBuzz/src/FizzBuzz.java
+++ FizzBuzz/src/FizzBuzz.java
@@ -4,14 +4,13 @@ public class FizzBuzz {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int number = scan.nextInt();
-        do {
-            System.out.println("Invalid number! Enter a positive number!");
-            number++;
-        } while (!scan.hasNextInt());
-        int newNumber = scan.nextInt();
-        for (int i = 0; i <= newNumber; i++) {
+        if (number <= 0) {
+            System.out.println("Give me a greater number than zero!");
+            System.err.println("Invalid number!");
+        }
+        for (int i = 1; i <= number; i++) {
             if (i % 3 == 0 && i % 5 == 0) {
-                System.out.println("The input is valid: " + newNumber + "\n" + "FizzBuzz");
+                System.out.println("FizzBuzz");
             } else if (i % 3 == 0) {
                 System.out.println("Fizz");
             } else if (i % 5 == 0) {
--- Strange Polynomial Sum/src/StrangePolynomialSum.java
+++ Strange Polynomial Sum/src/StrangePolynomialSum.java
@@ -4,14 +4,16 @@ public class StrangePolynomialSum {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int n = scan.nextInt();
-        if(n < 1){
+        if (n < 1) {
             System.err.println("Invalid number! Exit the program");
             System.exit(0);
         }
-        double sum = Math.pow(n,n);
-        for (int i = 1; i < n ; i++) {
-            sum += i;
-            i++;
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            if (i % 11 == 0) {
+                int exponent = (i / 11) + 1;
+                sum += Math.pow(i, exponent);
+            }
         }
         System.out.println("The sum of the strange polynomal is " + sum);
         scan.close();
