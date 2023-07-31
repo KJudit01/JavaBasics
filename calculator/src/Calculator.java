4ff18145d48b5ffe147156718341936d90bd228a
Kiss Judit <kijudit77@gmail.com>
2023-07-31 11:54:01 +0200

    Calculator

--- calculator/src/Calculator.java
+++ calculator/src/Calculator.java
@@ -1,5 +1,61 @@
-public class Calculator {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Calculator {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(getSum(10, 20));
+        System.out.println(getSubtraction(200, 150));
+        System.out.println(getMultiplication(100, 100));
+        System.out.println(getDivision(200, 50));
+        System.out.println(getDivision(1000, 3));
+        System.out.println(getModularDivision(10, 5));
+        System.out.println(getPower(10, 2));
+        System.out.println(getPower(300, 3));
+        System.out.println(getPercentage(200, 20));
+        System.out.println(getPercentage(5000, 35));
+        System.out.println(getFactorial(5));
+        System.out.println(getFactorial(20));
+        System.out.println(getSquareRoot(25));
+    }
+
+    static int getSum(int number1, int number2) {
+        return number1 + number2;
+    }
+
+    static int getSubtraction(int number1, int number2) {
+        return number1 - number2;
+    }
+
+    static int getMultiplication(int num1, int num2) {
+        return num1 * num2;
+    }
+
+    static int getDivision(int num1, int num2) {
+        return num1 / num2;
+    }
+
+    static int getModularDivision(int num1, int num2) {
+        return num1 % num2;
+    }
+
+    static double getPower(int num1, int num2) {
+        return Math.pow(num1, num2);
+    }
+
+    static int getPercentage(int num1, int num2) {
+        return num1 * num2 / 100;
+    }
+
+    static long getFactorial(int number) {
+        long factorial = 1;
+        for (int i = 1; i <= number; i++) {
+            factorial *= i;
+        }
+        return factorial;
+    }
+
+    static double getSquareRoot(int number) {
+        return Math.sqrt(number);
     }
 }
---
+++ calculator/src/CalculatorTest.java
@@ -0,0 +1,50 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+    @Test
+    void testGetSum() {
+        Assertions.assertEquals(200, Calculator.getSum(100, 100));
+        Assertions.assertEquals(1000, Calculator.getSum(100, 900));
+    }
+
+    @Test
+    void testGetSubtraction() {
+        Assertions.assertEquals(500, Calculator.getSubtraction(1000, 500));
+    }
+
+    @Test
+    void testGetMultiplication() {
+        Assertions.assertEquals(25, Calculator.getMultiplication(5, 5));
+    }
+
+    @Test
+    void testGetDivision() {
+        Assertions.assertEquals(10, Calculator.getDivision(100, 10));
+    }
+
+    @Test
+    void testModularDivision() {
+        Assertions.assertEquals(0, Calculator.getModularDivision(25, 5));
+    }
+
+    @Test
+    void testGetPower() {
+        Assertions.assertEquals(100, Calculator.getPower(10, 2));
+    }
+
+    @Test
+    void testGetPercentage() {
+        Assertions.assertEquals(200, Calculator.getPercentage(1000, 20));
+    }
+
+    @Test
+    void testGetFactorial() {
+        Assertions.assertEquals(120, Calculator.getFactorial(5));
+    }
+
+    @Test
+    void testGetSquareRoot() {
+        Assertions.assertEquals(4, Calculator.getSquareRoot(16));
+    }
+}
