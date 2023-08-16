19af54621687abb85c5e0f4f6540d86a49855c09
Kiss Judit <kijudit77@gmail.com>
2023-08-16 13:39:10 +0200

    String - Small Exercise

--- String-Small Exercise/src/StringSmallExerciseTest.java
+++ String-Small Exercise/src/StringSmallExerciseTest.java
@@ -3,14 +3,15 @@ import org.junit.jupiter.api.Test;
 
 public class StringSmallExerciseTest {
     @Test
-    void getHelloOccuranceTest(){
-        Assertions.assertEquals(0,StringSmallExercise.getHelloOccurance("lovely string"));
-        Assertions.assertEquals(4,StringSmallExercise.getHelloOccurance("helloabcdhelloasvbhellogdvmhello"));
-        Assertions.assertEquals(1,StringSmallExercise.getHelloOccurance("helloBéla"));
+    void getHelloOccuranceTest() {
+        Assertions.assertEquals(0, StringSmallExercise.getHelloOccurance("lovely string"));
+        Assertions.assertEquals(4, StringSmallExercise.getHelloOccurance("helloabcdhelloasvbhellogdvmhello"));
+        Assertions.assertEquals(1, StringSmallExercise.getHelloOccurance("helloBéla"));
     }
+
     @Test
-    void getOccuranceOfBsTest(){
-        Assertions.assertEquals(2,StringSmallExercise.getOccuranceOfBs("bBffFDGddbB"));
-        Assertions.assertEquals(0,StringSmallExercise.getOccuranceOfBs("abc"));
+    void getOccuranceOfBsTest() {
+        Assertions.assertEquals(2, StringSmallExercise.getOccuranceOfBs("bBffFDGddbB"));
+        Assertions.assertEquals(0, StringSmallExercise.getOccuranceOfBs("abc"));
     }
 }
